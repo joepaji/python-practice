227e01be4632065d00d5fc87712e2023b1eae81d
joepaji <rjoheb@hotmail.com>
2022-10-22 11:21:25 -0700

    include error handling

--- NATO_alphabet/main.py
+++ NATO_alphabet/main.py
@@ -6,6 +6,14 @@ df = pandas.read_csv('nato_phonetic_alphabet.csv')
 dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ")
-phonetic_code = [dict[char.upper()] for char in word]
-print(phonetic_code)
+def generate_phonetic():
+    word = input("Enter a word: ")
+    try:
+        phonetic_code = [dict[char.upper()] for char in word]
+    except KeyError: 
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_code)
+
+generate_phonetic()
